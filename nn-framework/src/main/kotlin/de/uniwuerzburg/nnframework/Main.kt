9b3a544d75bf6d7562a583f4882b886cc7b08430
festplatte <mich.gabler@gmx.de>
2019-06-13 22:19:55 +0200

    Add InputLayer for image strings, reading mnist in Main

--- nn-framework/src/main/kotlin/de/uniwuerzburg/nnframework/Main.kt
+++ nn-framework/src/main/kotlin/de/uniwuerzburg/nnframework/Main.kt
@@ -1,5 +1,27 @@
 package de.uniwuerzburg.nnframework
 
+import java.io.File
+
 fun main(args: Array<String>) {
-    println("Hello World!")
+    val mnistData = readFiles("/path/to/mnistfiles")
+    val img = mnistData.keys.first()
+    println(img)
+    println(mnistData[img])
+
+}
+
+fun readFiles(path: String): Map<String, Int> {
+    val dir = File(path)
+    val result = LinkedHashMap<String, Int>()
+    if (dir.isDirectory) {
+        dir.listFiles().filter { !it.isDirectory && it.absolutePath.endsWith(".image") }.forEach { imageFile ->
+            val labelFile = File(imageFile.absolutePath.replace(".image", ".label"))
+            if (labelFile.exists()) {
+                val image = imageFile.readText()
+                val label = labelFile.readText().toInt()
+                result.put(image, label)
+            }
+        }
+    }
+    return result
 }
---
+++ nn-framework/src/main/kotlin/de/uniwuerzburg/nnframework/layers/ImageStringInputLayer.kt
@@ -0,0 +1,26 @@
+package de.uniwuerzburg.nnframework.layers
+
+import de.uniwuerzburg.nnframework.data.Shape
+import de.uniwuerzburg.nnframework.data.Tensor
+
+/**
+ * Transformiert Strings mit Helligkeitwerten in Tensoren.
+ */
+class ImageStringInputLayer: InputLayer<String> {
+    private val ROW_SPLIT = System.lineSeparator()
+    private val COLUMN_SPLIT = " "
+
+    override fun forward(rawDataList: List<String>): List<Tensor> {
+        return rawDataList.map { data ->
+            val valuesList = data.split(ROW_SPLIT).map { row -> row.split(COLUMN_SPLIT).map { it.toFloat() } }
+            val shape = Shape(intArrayOf(valuesList.size, valuesList[0].size))
+            val valuesArray = FloatArray(shape.volume)
+            var index = 0
+            valuesList.forEach { row ->
+                System.arraycopy(row.toFloatArray(), 0, valuesArray, index, row.size)
+                index += row.size
+            }
+            Tensor(shape, valuesArray)
+        }
+    }
+}
