0bc0f907d5068758b5f2ea23aa5fbe9c70047dcd
festplatte <mich.gabler@gmx.de>
2019-06-18 23:57:13 +0200

    Add accuracy to testing

--- nn-framework/src/main/kotlin/de/uniwuerzburg/nnframework/SGDTrainer.kt
+++ nn-framework/src/main/kotlin/de/uniwuerzburg/nnframework/SGDTrainer.kt
@@ -2,6 +2,7 @@ package de.uniwuerzburg.nnframework
 
 import de.uniwuerzburg.nnframework.data.Tensor
 import de.uniwuerzburg.nnframework.loss.LossFkt
+import kotlin.math.exp
 
 class SGDTrainer(private val batchSize: Int = 1,
                  private val learningRate: Float,
@@ -31,12 +32,13 @@ class SGDTrainer(private val batchSize: Int = 1,
 
                 val forwardOutput = network.forward(batchData)
                 val loss = lossFkt.calculate(forwardOutput, batchLabels)
+                val accuracy = calcAccuracy(forwardOutput, batchLabels)
                 lossFkt.differentiate(forwardOutput, batchLabels)
                 network.backprop(forwardOutput)
                 // TODO update weights
                 network.updateWeights(updateMechanism, learningRate)
 
-                println("Epoch: $epoch - Data: $processedData/${dataList.size} - Loss: $loss")
+                println("Epoch: $epoch - Data: $processedData/${dataList.size} - Loss: $loss - Accuracy: $accuracy")
 
                 batchOffset += batchSize
             }
@@ -45,5 +47,31 @@ class SGDTrainer(private val batchSize: Int = 1,
         println("Training finished")
     }
 
+    /**
+     * Berechnet die Accuracy anhand der gegebenen und erwarteten Tensoren.
+     */
+    private fun calcAccuracy(actual: List<Tensor>, expected: List<Tensor>): Float {
+        var correctlyPredicted = 0
+        for (i in 0 until actual.size) {
+            if (getHighestIndex(actual[i]) == getHighestIndex(expected[i])) {
+                correctlyPredicted++
+            }
+        }
+        return correctlyPredicted.toFloat() / actual.size.toFloat()
+    }
 
+    /**
+     * Gibt den Index des größten Tensor-Elements zurück.
+     */
+    private fun getHighestIndex(tensor: Tensor): Int {
+        var highest: Float = 0f
+        var highestIndex: Int = 0
+        tensor.elements.forEachIndexed { index, fl ->
+            if (fl > highest) {
+                highest = fl
+                highestIndex = index
+            }
+        }
+        return highestIndex
+    }
 }
