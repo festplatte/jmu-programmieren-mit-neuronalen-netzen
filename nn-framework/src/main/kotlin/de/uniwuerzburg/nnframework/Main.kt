9b2cecb4be6f8813587fc4069c2b0330911f712a
Simon Englert <simon.englert@stud-mail.uni-wuerzburg.de>
2019-06-25 18:10:51 +0200

    merged my stash

--- nn-framework/src/main/kotlin/de/uniwuerzburg/nnframework/Main.kt
+++ nn-framework/src/main/kotlin/de/uniwuerzburg/nnframework/Main.kt
@@ -9,9 +9,11 @@ import java.io.File
 
 fun main(args: Array<String>) {
 //    val mnistTrain = readFiles("C:/Users/Simon Englert/Documents/Studium/ML for NLP/MNIST PyTorch/data/train")
-//    val mnistTest = readFiles("C:/Users/Simon Englert/Documents/Studium/ML for NLP/MNIST PyTorch/data/test")
-    val mnistTrain = readFiles("/Users/michaelgabler/Repositories/jmu-machine-learning-for-nlp/exercise-3/MNIST PyTorch/data/train")
-    val mnistTest = readFiles("/Users/michaelgabler/Repositories/jmu-machine-learning-for-nlp/exercise-3/MNIST PyTorch/data/test")
+//    val mnistTrain = readFiles("C:/Users/Simon Englert/Documents/Studium/ML for NLP/MNIST PyTorch/data/test")
+//    val mnistTrain = readFiles("C:/Users/simon/Documents/Master/MNIST PyTorch/data/train2")
+    val mnistTest = readFiles("C:/Users/simon/Documents/Master/MNIST PyTorch/data/test")
+//    val mnistTrain = readFiles("/Users/michaelgabler/Repositories/jmu-machine-learning-for-nlp/exercise-3/MNIST PyTorch/data/train")
+//    val mnistTest = readFiles("/Users/michaelgabler/Repositories/jmu-machine-learning-for-nlp/exercise-3/MNIST PyTorch/data/test")
 
     val cnNetwork = Network(ImageStringInputLayer(), listOf(
             Conv2DLayer(Shape(intArrayOf(28, 28, 1)), Shape(intArrayOf(27, 27, 4)), Shape(intArrayOf(2, 2, 1)), 4),
@@ -27,9 +29,10 @@ fun main(args: Array<String>) {
             SoftmaxLayer(Shape(intArrayOf(1, 10)))
     ))
 
-    val trainer = SGDTrainer(256, 0.01f, 5, CrossEntropyLoss(), true, SGDFlavor.STOCHASTIC_GRADIENT_DESCENT)
-    trainer.optimize(fcNetwork, mnistTrain)
-    trainer.validate(fcNetwork, mnistTest)
+    val trainer = SGDTrainer(256, 0.0001f, 20, CrossEntropyLoss(), true, SGDFlavor.STOCHASTIC_GRADIENT_DESCENT)
+    trainer.optimize(cnNetwork, mnistTest)
+    trainer.validate(cnNetwork, mnistTest)
+
 }
 
 fun readFiles(path: String): Map<String, Tensor> {
